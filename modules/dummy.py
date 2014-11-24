2bf995d6b8a74bff28c5cae8d07ff91e2e06fa4a
Matthew Paisner <matthew.paisner@gmail.com>
2014-11-24 11:05:28 -0500

    git is being weird.

--- modules/dummy.py
+++
@@ -1,9 +0,0 @@
-
-class Dummy:
-	
-	def init(self, world, mem, memKeys):
-		self.mem = mem
-		self.memKeys = memKeys
-	
-	def run(self, verbose):
-		print "Hello World!"
--- modules/observe.py
+++ modules/observe.py
@@ -1,4 +1,3 @@
-from utils import blockstate, scene
 
 class PerfectOberver:
 
