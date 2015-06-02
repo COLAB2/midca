bbda8b3d8986fa8957b66046d49d2fc6cf9bbf17
Dustin <dustin.td@gmail.com>
2015-06-02 11:13:55 -0400

    intend module now follows new trace collection

--- modules/intend.py
+++ modules/intend.py
@@ -6,20 +6,23 @@ class SimpleIntend:
 	
 	#if new goal was generated since last run, set current goal to it. Otherwise, set current goal to None to stop redundant planning.
 	def run(self, cycle, verbose = 2):
-                trace_str = "INPUT:\n"
+                trace = self.mem.trace
+                if trace:
+                        trace.add_phase(cycle,self.__class__.__name__)
+                        trace.add_data("GOALGRAPH",copy.deepcopy(self.mem.GOAL_GRAPH))
+                        
 		goalGraph = self.mem.get(self.mem.GOAL_GRAPH)
-                trace_str += "  goalgraph: "+str(self.mem.GOAL_GRAPH)
+
 		if not goalGraph:
 			if verbose >= 1:
 				print "Goal graph not initialized. Intend will do nothing."
 			return
 		goals = goalGraph.getUnrestrictedGoals()
 		self.mem.set(self.mem.CURRENT_GOALS, goals)
-                trace_str += "\nOUTPUT:\n"
-                trace_str += "  Current goals are now:\n "
-                if goals:
-                        for g in goals:
-                                trace_str += "    " + str(g) + "\n"
+
+                if trace:
+                        trace.add_data("GOALS",goals)
+                                
 		if not goals:
 			if verbose >= 2:
 				print "No goals selected."
@@ -30,6 +33,4 @@ class SimpleIntend:
 					print goal,
 				print
 
-                trace = self.mem.trace
-                if trace:
-                        trace.addphase(cycle,self.__class__.__name__,trace_str)
+
