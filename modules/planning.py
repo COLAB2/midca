d666812fe1f42de1d666b4401b0f0354e5733294
Zohreh <zohreh.alavi@gmail.com>
2016-12-20 13:35:45 -0500

    jshop is added in planning.py

--- modules/planning.py
+++ modules/planning.py
@@ -1,6 +1,7 @@
 from _plan import pyhop
 from MIDCA import plans, base
 from MIDCA.modules._plan.asynch import asynch
+from MIDCA.modules._plan.jShop import JSHOP
 from MIDCA.modules._plan.pyhop import print_state,  print_methods, print_operators
 import collections
 import traceback
@@ -171,6 +172,124 @@ class AsynchPyhopPlanner(GenericPyhopPlanner):
         #save new plan
         if asynchPlan != None:
             self.mem.get(self.mem.GOAL_GRAPH).addPlan(asynchPlan)
+
+
+
+class JSHOPPlanner(base.BaseModule):
+    '''
+    MIDCA module that implements a python version of the SHOP hierarchical task network (HTN) planner. HTN planners require a set of user-defined methods to generate plans; these are defined in the methods python module and declared in the constructor for this class.
+    Note that this module uses has several methods to translate between MIDCA's world and goal representations and those used by pyhop; these should be changed if a new domain is introduced.
+    '''
+
+    jshop_state_from_world = None
+    jshop_tasks_from_goals = None
+    
+    def __init__(self,
+                 jshop_state_from_world,
+                 jshop_tasks_from_goals,
+                 extinguishers = False,
+                 mortar = False):
+        
+        self.jshop_state_from_world = jshop_state_from_world
+        self.jshop_tasks_from_goals = jshop_tasks_from_goals
+        
+        try:
+            self.working = True
+        except:
+            print "Error declaring pyhop methods and operators. This planner will be \
+            disabled"
+            traceback.print_exc()
+            self.working = False
+      
+    def init(self, world, mem):
+        self.world = world
+        self.mem = mem
+        self.mem.set(self.mem.PLANNING_COUNT, 0)  
+    #this will require a lot more error handling, but ignoring now for debugging.
+    def run(self, cycle, verbose = 2):
+        world = self.mem.get(self.mem.STATES)[-1]
+        goals = self.mem.get(self.mem.CURRENT_GOALS)
+
+        trace = self.mem.trace
+        if trace:
+            trace.add_module(cycle,self.__class__.__name__)
+            trace.add_data("WORLD", copy.deepcopy(world))
+            trace.add_data("GOALS", copy.deepcopy(goals))
+
+        if not goals:
+            if verbose >= 2:
+                print "No goals received by planner. Skipping planning."
+            return
+        try:
+            midcaPlan = self.mem.get(self.mem.GOAL_GRAPH).getMatchingPlan(goals)
+        except AttributeError:
+            midcaPlan = None
+        if midcaPlan:
+            if verbose >= 2:
+                print "Old plan retrieved. Checking validity...",
+            valid = world.plan_correct(midcaPlan)
+            if not valid:
+                midcaPlan = None
+                #if plan modification is added to MIDCA, do it here.
+                if verbose >= 2:
+                    print "invalid."
+            elif verbose >= 2:
+                print "valid."
+            if valid:
+                if verbose >= 2:
+                    print "checking to see if all goals are achieved...",
+                achieved = world.plan_goals_achieved(midcaPlan)
+                if verbose >= 2:
+                    if len(achieved) == len(midcaPlan.goals):
+                        print "yes"
+                    else:
+                        print "no. Goals achieved: " + str({str(goal) for goal in achieved})
+                if len(achieved) != len(midcaPlan.goals):
+                    midcaPlan = None #triggers replanning.
+
+        #ensure goals is a collection to simplify things later.
+        if not isinstance(goals, collections.Iterable):
+            goals = [goals]
+
+        if not midcaPlan:
+            #use pyhop to generate new plan
+            if verbose >= 2:
+                print "Planning..."
+#             try:
+            jshopState = self.jshop_state_from_world(world)
+#             except Exception:
+#                 print "Could not generate a valid pyhop state from current world state. Skipping planning"
+#             try:
+            jshopTasks = self.jshop_tasks_from_goals(goals,jshopState)
+#             except Exception:
+#                 print "Could not generate a valid pyhop task from current goal set. Skipping planning"
+            try:
+                self.mem.set(self.mem.PLANNING_COUNT, 1+self.mem.get(self.mem.PLANNING_COUNT))
+                jshopPlan = JSHOP.jshop(jshopTasks)
+            except Exception:
+                jshopPlan = None
+            if not jshopPlan and jshopPlan != []:
+                if verbose >= 1:
+                    print "Planning failed for ",
+                    for goal in goals:
+                        print goal, " ",
+                    print
+                if trace: trace.add_data("PLAN", jshopPlan)
+                return
+            #change from pyhop plan to MIDCA plan
+            midcaPlan = plans.Plan([plans.Action(action[0], *list(action[1:])) for action in jshopPlan], goals)
+            
+            if verbose >= 1:
+                print "Planning complete."
+            if verbose >= 2:
+                print "Plan: "#, midcaPlan
+                for a in midcaPlan:
+                    print("  "+str(a))
+            #save new plan
+            if midcaPlan != None:
+                self.mem.get(self.mem.GOAL_GRAPH).addPlan(midcaPlan)
+            if trace: trace.add_data("PLAN",midcaPlan)
+
 
 class PyHopPlanner(base.BaseModule):
 
